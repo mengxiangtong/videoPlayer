5125d534e3fac4682f4e32f94664f5aa3b2556ce
928593829@qq.com <928593829@qq.com>
2018-07-13 09:28:09 +0800

    11

---
+++ VideoPlayerDemo/VideoPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayerDemo
+//
+//  Created by xhkj on 2018/7/13.
+//  Copyright © 2018年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoPlayerDemo/VideoPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayerDemo
+//
+//  Created by xhkj on 2018/7/13.
+//  Copyright © 2018年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
